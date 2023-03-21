940ab99949033ba31e4b9a684184029dbd31cc90
姚烁 <yaoshuo@yaoshuodeMacBook-Pro.local>
2023-03-21 16:12:47 +0800

    hexagons.py

--- hexagons.py
+++ hexagons.py
@@ -3,7 +3,7 @@ def hexagon_number(n):
     # The formula for the nth hexagon number is H(n) = n(2n-1), where n is the index
     return n * (2 * n - 1)
 
-# Use a loop to compute and print the first five hexagon numbers
+# Use a loop to compute and print the first five hexagon numbers.
 for i in range(1, 6):
     # Compute the ith hexagon number
     hex_num = hexagon_number(i)
