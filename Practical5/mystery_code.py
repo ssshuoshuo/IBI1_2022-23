67620e0cf14901e4714c909384c28301122ca89d
姚烁 <yaoshuo@yaoshuodeMacBook-Pro.local>
2023-03-24 17:23:19 +0800

    mystery_code.py

---
+++ Practical5/mystery_code.py
@@ -0,0 +1,24 @@
+# What does this piece of code do?
+# Answer:This code generates a random number and compares it with all 
+previously generated random numbers, storing the maximum random number in 
+the variable stored_random_number, and loops until 10 random numbers have 
+been generated.
+
+# Import libraries
+# randint allows drawing a random number,
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil
+
+progress=0
+stored_random_number=0
+while progress<10:
+	progress+=1
+	n = randint(1,100)
+	if n > stored_random_number:
+		stored_random_number = n
+
+print(stored_random_number)
