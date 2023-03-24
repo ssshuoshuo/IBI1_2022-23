53bce5b6cd8f156282d0d20012a11a71bb7ed8e6
姚烁 <yaoshuo@yaoshuodeMacBook-Pro.local>
2023-03-24 17:19:21 +0800

    olympiccost.py

--- Practical6/olympiccost.py
+++ Practical6/olympiccost.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
-
+#creat a list costs
 costs = [1,8,15,7,5,14,43,40]
 sorted_costs = sorted(costs)
-
+#sort the costs in ascending order
 print("Sorted costs:", sorted_costs)
-
+#print(sorted_costs)
 colors = ['r', 'coral', 'gold', 'c', 'chartreuse', 'plum', 'pink']
-
+#creat a bar plot of the sorted cost
 plt.bar(range(len(sorted_costs)), sorted_costs, color=colors)
 plt.xlabel("Olympic Games")
 plt.ylabel("Cost (in $ billions)")
