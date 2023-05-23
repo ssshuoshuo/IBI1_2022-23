4ed9f7d7d5ad90430d814b6b1f0dd2b8aca94aa3
姚烁 <yaoshuo@yaoshuodeMacBook-Pro.local>
2023-05-23 11:12:25 +0800

    Triathlon_times_database.py

--- Practical10/Triathlon_times_database.py
+++ Practical10/Triathlon_times_database.py
@@ -10,10 +10,7 @@ cycle_time, run_time):
         self.total_time = swim_time + cycle_time + run_time
 
     def speak(self):
-        print("Name: {} {}; Location: {}; Swim Time: {}s; Cycle Time: {}s; 
-Run Time: {}s; Total Time: {}s".format(
-            self.first_name, self.last_name, self.location, 
-self.swim_time, self.cycle_time, self.run_time, self.total_time))
+        print("Name: {} {}; Location: {}; Swim Time: {}s; Cycle Time: {}s; Run Time: {}s; Total Time: {}s".format(self.first_name, self.last_name, self.location, self.swim_time, self.cycle_time, self.run_time, self.total_time))
 
 
 # Example usage
